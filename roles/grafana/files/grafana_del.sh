99fd8c0fff488aa03e40e7cf9be89110f4ceeec0
MJeyhun <mustafayevjeyhun0@gmail.com>
2025-11-02 17:10:05 +0200

    Lab 7 part 2

---
+++ roles/grafana/files/grafana_del.sh
@@ -0,0 +1,7 @@
+apt-get purge --auto-remove grafana
+sudo rm -rf /etc/grafana/          # Config files
+sudo rm -rf /var/lib/grafana/       # Data files, plugins, etc.
+sudo rm -rf /var/log/grafana/       # Log files
+sudo rm -rf /usr/share/grafana/     # Static files
+sudo userdel grafana
+sudo groupdel grafana
